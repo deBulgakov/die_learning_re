56d5c5a9ff7513357cf6a3df34419ad0d1e6dac6
Blop <claire.gestalt@gmail.com>
2024-10-19 03:15:51 +0300

    Updated limits

--- compare_re.py
+++ compare_re.py
@@ -1,23 +1,26 @@
 import exrex
 import re
+import math
 
-a = r'a*b*|a(a|b)*|bb' # Введите свое регулярное выражение
-b = r'a(a|b)*|bb|1' # Введите регулярное выражение для сравнения
-
+a = r'a*b*|a(a|b)*|bb'  # Введите свое регулярное выражение
+b = r'a(a|b)*|bb|1'  # Введите регулярное выражение для сравнения
+alph = 'abcdef'
 
 def main():
     i = 0
-    for test in exrex.generate(b, 110):
+    n_used = len(set([i for i in alph if i in a or i in b]))
+    limit_tests = 100 ** (max(a.count('|'), b.count('|'), n_used))
+    limit_generated = math.ceil(math.log(limit_tests, n_used))
+    for test in exrex.generate(b, limit_generated):
         if re.match(a, test) is None:
             print(test)
             print('Not matched')
             return
         i += 1
-        if i == 1000000:
+        if i == limit_tests:
             print('All gut')
             return
     print(i)
-
 
 
 if __name__ == '__main__':
